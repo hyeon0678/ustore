bfd1ad2bf9db9e6e76504cd2005f1839f0c93cf9
oh_hyeon_bi <hyi067811@gmail.com>
2023-12-27 09:36:02 +0900

    로그인 권한 로직 수정

--- src/main/java/com/ustore/main/dto/EmployeeUserDto.java
+++ src/main/java/com/ustore/main/dto/EmployeeUserDto.java
@@ -3,7 +3,36 @@ package com.ustore.main.dto;
 public class EmployeeUserDto {
 	private String empIdx;
 	private String empPw;
+	private String positionName;
+	private String deptName;
+	private int parentDeptId;
 	
+	
+	
+	public String getPositionName() {
+		return positionName;
+	}
+
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public int getParentDeptId() {
+		return parentDeptId;
+	}
+
+	public void setParentDeptId(int parentDeptId) {
+		this.parentDeptId = parentDeptId;
+	}
+
 	public EmployeeUserDto(String empIdx, String empPw) {
 		this.empIdx = empIdx;
 		this.empPw = empPw;
