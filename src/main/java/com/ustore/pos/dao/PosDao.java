4338d1eb727bbc777eb2c99f9c4434f005e12af1
lim_jae_hyeong <wogud822@gmail.com>
2024-01-02 17:09:16 +0900

    pos기 물품 리스트 불러오기, 회원별 장바구니 기능

--- src/main/java/com/ustore/pos/dao/PosDao.java
+++ src/main/java/com/ustore/pos/dao/PosDao.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ustore.member.dto.MemberDto;
+import com.ustore.pos.dto.PosDto;
 
 @Mapper
 public interface PosDao {
@@ -13,5 +14,21 @@ public interface PosDao {
 	ArrayList<MemberDto> posMember();
 
 	ArrayList<MemberDto> memberSearch(HashMap<String, String> params);
+
+	ArrayList<PosDto> posItem();
+
+	ArrayList<PosDto> posCart(String memberIdx);
+
+	int updateCart(HashMap<String, String> params);
+
+	void posCartInsert(HashMap<String, String> params);
+
+	void deleteCart(HashMap<String, String> params);
+
+	int selQuantity(HashMap<String, String> params);
+
+	String selInsert(HashMap<String, String> params);
+
+	ArrayList<PosDto> productSearch(HashMap<String, String> params);
 
 }
